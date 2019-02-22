3bf6e003a228be50ba8d644027f25bbb300ee70c
Islam Soliman <o-islam.msoliman@etisalat.com>
2019-02-23 00:43:31 +0200

    Initial Commit

---
+++ Instabug_Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instabug_Task
+//
+//  Created by Islam Soliman on 2/23/19.
+//  Copyright © 2019 Islam Soliman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
